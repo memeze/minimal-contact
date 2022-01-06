33d79410a97364a49ebe6eba3a750bd5df9c1aca
Minjae Kim <alswo5052@gmail.com>
2022-01-06 13:42:57 +0900

    feat: Add surface top app bar composable

---
+++ app/src/main/java/com/bemily/samplecontact/ui/component/SurfaceTopAppBar.kt
@@ -0,0 +1,26 @@
+package com.bemily.samplecontact.ui.component
+
+import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.insets.statusBarsPadding
+
+@Composable
+fun SurfaceTopAppBar(
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable (() -> Unit)? = null,
+    title: @Composable () -> Unit
+) {
+    Surface(elevation = 4.dp) {
+        TopAppBar(
+            modifier = modifier.statusBarsPadding(),
+            backgroundColor = Color.Transparent,
+            elevation = 0.dp,
+            navigationIcon = navigationIcon,
+            title = title
+        )
+    }
+}
