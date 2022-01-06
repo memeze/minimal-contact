7f540a2493217d2f1c87bc83a034f7a0b601bf8d
Minjae Kim <alswo5052@gmail.com>
2022-01-06 14:07:41 +0900

    feat: Set contact list

--- app/src/main/java/com/bemily/samplecontact/ui/ContactScreen.kt
+++ app/src/main/java/com/bemily/samplecontact/ui/ContactScreen.kt
@@ -1,7 +1,13 @@
 package com.bemily.samplecontact.ui
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -11,12 +17,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.bemily.samplecontact.R
+import com.bemily.samplecontact.data.model.Contact
 import com.bemily.samplecontact.ui.component.SurfaceTopAppBar
 import com.bemily.samplecontact.ui.theme.SampleContactTheme
 
 @Composable
 fun ContactScreen(modifier: Modifier = Modifier) {
+    val lazyListState = rememberLazyListState()
+    val contactList: List<Contact> = Contact.getMockContactList()
+
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
@@ -31,7 +42,42 @@ fun ContactScreen(modifier: Modifier = Modifier) {
                 }
             )
         }
-    ) { }
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            state = lazyListState
+        ) {
+            items(contactList) { item ->
+                ContactItem(
+                    name = item.name,
+                    phoneNumber = item.phoneNumber
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ContactItem(
+    name: String = "",
+    phoneNumber: String = ""
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.h5,
+            color = MaterialTheme.colors.onSurface
+        )
+        Text(
+            text = phoneNumber,
+            style = MaterialTheme.typography.subtitle1,
+            color = MaterialTheme.colors.onSurface.copy(alpha = .5f)
+        )
+    }
 }
 
 @Preview("light theme", uiMode = Configuration.UI_MODE_NIGHT_NO)
