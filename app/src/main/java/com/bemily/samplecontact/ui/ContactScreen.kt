b34f8508b26b06c0016bafe25517fd6dc54055e4
Minjae Kim <alswo5052@gmail.com>
2022-01-07 17:49:43 +0900

    chore: Set phone number display format

--- app/src/main/java/com/bemily/samplecontact/ui/ContactScreen.kt
+++ app/src/main/java/com/bemily/samplecontact/ui/ContactScreen.kt
@@ -2,6 +2,7 @@ package com.bemily.samplecontact.ui
 
 import android.Manifest
 import android.content.res.Configuration
+import android.telephony.PhoneNumberUtils
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bemily.samplecontact.R
@@ -72,7 +74,7 @@ fun ContactScreen(modifier: Modifier = Modifier) {
                     items(contactList) { item ->
                         ContactItem(
                             name = item.name,
-                            phoneNumbers = item.phoneNumbers
+                            phoneNumbers = item.phoneNumbers.map { PhoneNumberUtils.formatNumber(it, Locale.current.region) }
                         )
                         Divider()
                     }
